11fe0e2520ec840dee8d8534b90e060fca85ded3
mei <yimei.wym@gmail.com>
2019-05-20 21:27:16 +0800

    add assets bundles

--- cust_app_web/util/assets.py
+++ cust_app_web/util/assets.py
@@ -1,3 +1,20 @@
 from flask_assets import Bundle
 
-bundles = {}
+bundles = {
+    'home_js': Bundle(
+        'js/vendor/jquery-3.3.1.js',
+        'js/vendor/popper.js',
+        'js/vendor/bootstrap-4.1.3.js',
+        'js/custom.js',
+        filters='jsmin',
+        output='gen/home.%(version)s.js'),
+
+    'home_css': Bundle(
+        'css/vendor/bootstrap-4.1.3.css',
+        'css/user.css',
+        'css/upload.css',
+        'css/custom.css',
+        'css/homepage.css',
+        filters='cssmin',
+        output='gen/home.%(version)s.css')
+}
