0f0571cd98556f31866d4d8486094d207da231d3
mei <yimei.wym@gmail.com>
2019-05-23 18:26:41 +0800

    build time slot api

--- cust_app_api/__init__.py
+++ cust_app_api/__init__.py
@@ -8,9 +8,14 @@ from app import app
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
-# API Routes
+## API Routes
+from cust_app_api.blueprints.users.views import users_api_blueprint
+from cust_app_api.blueprints.sessions.views import sessions_api_blueprint
+from cust_app_api.blueprints.timeslots.views import timeslots_api_blueprint
 
 app.register_blueprint(images_api_blueprint, url_prefix='/api/v1/images')
 app.register_blueprint(orders_api_blueprint, url_prefix='/api/v1/orders')
 app.register_blueprint(sessions_api_blueprint, url_prefix='/api/v1')
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
+app.register_blueprint(sessions_api_blueprint, url_prefix='/api/v1')
+app.register_blueprint(timeslots_api_blueprint, url_prefix='/api/v1/timeslots')
--- cust_app_web/util/helpers/moderation.py
+++ cust_app_web/util/helpers/moderation.py
@@ -16,13 +16,11 @@
 
 #             results = response['outputs'][0]['data']['concepts']
 
-#             for i in range(len(results)):
-#                 if results[i]['value'] > 0.6:
-#                     if results[i]['name'] != 'safe':
-#                         res = {}
-#                         res['pict'] = url
-#                         res[results[i]['name']]= results[i]['value']
-#                         errors.append(res)
-#     return errors
-
-# moderate("https://www.google.com/search?q=synthwave&safe=active&rlz=1C1CHZL_esPE747PE747&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiT-taShLHiAhX26nMBHc1rCpgQ_AUIDigB&biw=1536&bih=750#imgrc=yzmcDtYmDKuMuM:")
+            for i in range(len(results)):
+                if results[i]['value'] > 0.6:
+                    if results[i]['name'] != 'safe':
+                        res = {}
+                        res['pict'] = url
+                        res[results[i]['name']]= results[i]['value']
+                        errors.append(res)
+    return errors
