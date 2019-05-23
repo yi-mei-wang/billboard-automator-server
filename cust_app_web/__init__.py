3da2cd4eb7a533f7bea2f493fef5679c7c2c6114
mei <yimei.wym@gmail.com>
2019-05-23 14:29:08 +0800

    handle new orders

--- cust_app_web/__init__.py
+++ cust_app_web/__init__.py
@@ -22,22 +22,24 @@ def home():
 
 @app.errorhandler(400)
 def bad_request(e):
-    response = jsonify({'code': 400, 'message': 'Image did not pass moderation test'})
-    response.status_code = 400
-    return response
+    # response = jsonify({'code': 400, 'message': 'Image did not pass moderation test'})
+    # response.status_code = 400
+    # return response
+    return render_template('400.html'), 400
 
 @app.errorhandler(403)
 def access_denied(e):
     return render_template('403.html'), 403
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-
 
 
 app.register_error_handler(400, bad_request)
