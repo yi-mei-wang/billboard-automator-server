77bc0e1cbb118d5d326a8ae075791bcb7a7bee48
mei <yimei.wym@gmail.com>
2019-05-26 13:09:01 +0800

    sign user in after they sign up

--- cust_app_api/blueprints/users/views.py
+++ cust_app_api/blueprints/users/views.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, jsonify, request
-from werkzeug.security import generate_password_hash
 from peewee import IntegrityError
+from werkzeug.security import generate_password_hash
+import datetime
+import jwt
 import sys
 import uuid
+
+from app import app
 from models.user import User
 
 
@@ -14,12 +18,12 @@ def index():
     return jsonify({'name': 'hello world'})
 
 
-@users_api_blueprint.route('/', methods=['POST'])
+@users_api_blueprint.route('/create', methods=['POST'])
 def create():
     data = request.get_json()
-
+    breakpoint()
     # Validation:
-    # 1. Check if all fields are keyed in 
+    # 1. Check if all fields are keyed in
     # 2. Length
     # 3. Password complexity
     # 4. Email validity
@@ -32,15 +36,19 @@ def create():
     # elif not data["password"]:
     #     return jsonify({'status': 400 , 'message': 'Password is required'})
 
+    hashed_password = generate_password_hash(
+        data['password'], method='sha256', salt_length=8)
 
-    hashed_password = generate_password_hash(data['password'], method='sha256', salt_length=8)
-
-    new_user = User(username=data["username"], password=hashed_password, email=data["email"], public_id=uuid.uuid4())
+    new_user = User(username=data["username"], password=hashed_password,
+                    email=data["email"], public_id=uuid.uuid4())
 
     # Give more specific error messages
     try:
         new_user.save()
-        return jsonify({'status': 201, 'message': 'User created'})
+        token = jwt.encode({'public_id': new_user.public_id, 'exp': datetime.datetime.utcnow(
+        ) + datetime.timedelta(minutes=30)}, app.config["SECRET_KEY"])
+
+        return jsonify({'auth_token': token.decode('UTF-8'), 'status': 201, 'message': 'User created', 'user': {'id': new_user.public_id, 'username': new_user.username}})
 
     # When username and email are taken
     except IntegrityError as e:
@@ -48,4 +56,3 @@ def create():
             return jsonify({'status': 409, 'message': 'Username is taken'})
         elif 'email' in str(e):
             return jsonify({'status': 409, 'message': 'Email is taken'})
-    
