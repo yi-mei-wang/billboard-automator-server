0d975b46f285280156db5c790f1c9263ab80684e
mei <yimei.wym@gmail.com>
2019-05-26 15:24:44 +0800

    resolve blueprints conflict

--- cust_app_api/__init__.py
+++ cust_app_api/__init__.py
@@ -1,10 +1,10 @@
 from flask_cors import CORS
 from app import app
-from cust_app_api.blueprints.images.views import images_api_blueprint
-from cust_app_api.blueprints.orders.views import orders_api_blueprint
-from cust_app_api.blueprints.sessions.views import sessions_api_blueprint
-from cust_app_api.blueprints.timeslots.views import timeslots_api_blueprint
 from cust_app_api.blueprints.users.views import users_api_blueprint
+from cust_app_api.blueprints.timeslots.views import timeslots_api_blueprint
+from cust_app_api.blueprints.sessions.views import sessions_api_blueprint
+from cust_app_api.blueprints.orders.views import orders_api_blueprint
+from cust_app_api.blueprints.images.views import images_api_blueprint
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
