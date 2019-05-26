d4f5d3dc27c8b5d08079391d3723926d67ad4ec6
mei <yimei.wym@gmail.com>
2019-05-26 13:09:01 +0800

    sign user in after they sign up

--- cust_app_api/blueprints/sessions/views.py
+++ cust_app_api/blueprints/sessions/views.py
@@ -26,6 +26,7 @@ def login():
     if check_password_hash(user.password, auth['password']):
         token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow(
         ) + datetime.timedelta(minutes=30)}, app.config["SECRET_KEY"])
-        return jsonify({'auth_token': token.decode('UTF-8'), 'message': "Successfully signed in", 'status': 201, 'user': {'id': user.id, 'username': user.username}})
+
+        return jsonify({'auth_token': token.decode('UTF-8'), 'message': "Successfully signed in", 'status': 201, 'user': {'id': user.public_id, 'username': user.username}})
 
     return make_response('Password incorrect', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
